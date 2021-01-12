19e91577d4a87bfe40598a658c95c36c83f9b95a
Jacob Aguilar <aguilar.campos.jacob@gmail.com>
2021-01-12 15:28:44 -0300

    Initial Commit

---
+++ Formularios/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Formularios
+//
+//  Created by Jacob Aguilar on 12-01-21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Formularios/FormulariosApp.swift
@@ -0,0 +1,17 @@
+//
+//  FormulariosApp.swift
+//  Formularios
+//
+//  Created by Jacob Aguilar on 12-01-21.
+//
+
+import SwiftUI
+
+@main
+struct FormulariosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
